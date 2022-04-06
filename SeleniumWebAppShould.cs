4c8123341779d2a4dd248d706703089493ce788c
Indygos <cyberindygos@gmail.com>
2022-04-06 14:13:19 +0200

    Add project files.

---
+++ SeleniumWebAppShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium.UITests
+{
+    public class SeleniumWebAppShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://allegro.pl");
+
+                string pageTitle = driver.Title;
+
+                Assert.Equal("Allegro - atrakcyjne ceny", pageTitle);
+            }
+        }
+    }
+}
