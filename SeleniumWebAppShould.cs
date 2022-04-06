d62a42c72f9ba4041fe5fdee630ec45b61933d95
Indygos <cyberindygos@gmail.com>
2022-04-06 23:02:34 +0200

    Selenium WebDriver - Load, Reload, Back, Forward Page

--- SeleniumWebAppShould.cs
+++ SeleniumWebAppShould.cs
@@ -7,17 +7,88 @@ namespace Selenium.UITests
 {
     public class SeleniumWebAppShould
     {
+
+        private const string HomeUrl = "https://allegro.pl/";
+        private const string AboutUrl = "https://about.allegro.eu/";
+        private const string HomeTitle = "Allegro - atrakcyjne ceny";
         [Fact]
         [Trait("Category", "Smoke")]
         public void LoadApplicationPage()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl("https://allegro.pl");
 
-                string pageTitle = driver.Title;
+                driver.Navigate().GoToUrl(HomeUrl);
 
-                Assert.Equal("Allegro - atrakcyjne ceny", pageTitle);
+                DemoHelper.Pause();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+                driver.Navigate().GoToUrl(HomeUrl);
+
+                DemoHelper.Pause();
+
+                driver.Navigate().Refresh();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnBack()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().GoToUrl(AboutUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+
+                //TODO: assert that page was reloaded
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnForward()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(AboutUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+
+                driver.Navigate().Forward();
+                DemoHelper.Pause();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+
+                //TODO: assert that page was reloaded
             }
         }
     }
